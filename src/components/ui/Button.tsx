67b45dfc1d7db6de81ecd3bf7f00f9caeb203b0b
edisonlin31 <edisonjnz31@gmail.com>
2025-10-03 13:36:23 +0700

    Update default and secondary button styles to use specific color codes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -13,10 +13,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(
           'inline-flex  items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
           {
-            'bg-primary text-white hover:bg-primary py-5': variant === 'default',
+            'bg-[#FF8800] text-white hover:bg-[#FF9933] py-5': variant === 'default',
             'bg-red-600 text-white hover:bg-red-700': variant === 'destructive',
             'border border-gray-300 bg-transparent hover:bg-gray-50': variant === 'outline',
-            'bg-secondary text-primary hover:bg-[#E2E8F0] border-0  py-5': variant === 'secondary',
+            'bg-secondary text-[#FF8800] hover:bg-[#E2E8F0] border-0  py-5': variant === 'secondary',
             'hover:bg-gray-100': variant === 'ghost',
             'h-10 py-2 px-4': size === 'default',
             'h-9 px-3 rounded-md': size === 'sm',
